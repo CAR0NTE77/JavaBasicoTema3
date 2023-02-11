5b393008595531723a34e5052259f1805fcd1ff7
CAR0NTE77 <122113571+CAR0NTE77@users.noreply.github.com>
2023-02-10 22:20:29 -0400

    Initial commit

---
+++ src/com/OpenBootcamp/Tema3.java
@@ -0,0 +1,14 @@
+package com.OpenBootcamp;
+
+public class Tema3 {
+    public static void main(String[] args) {
+
+        String[] nombres = {"juan", "luis", "pablo", "omar" };
+
+        System.out.println("los nombres del array son: ");
+
+        for (String nombre : nombres) {
+            System.out.print(nombre + " ");
+        }
+    }
+}
